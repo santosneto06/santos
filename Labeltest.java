0e5ccb536d5316114a1ad37be1a96461eeb5f47b
santosneto06 <160780590+santosneto06@users.noreply.github.com>
2024-02-21 16:17:19 -0300

    Create Labeltest.java

---
+++ Labeltest.java
@@ -0,0 +1,10 @@
+import javax.swing.JFrame;
+public class Labeltest {
+    public static void main(String[] args) {
+        LabelFrame labelFrame = new LabelFrame();
+        labelFrame.setDefaultCloseOperation( Jframe.EXIT_ON_CLOSE);
+        labelFrame.setSize(260, 180);
+        labelFrame.setVisible( true );
+    }
+    
+}
